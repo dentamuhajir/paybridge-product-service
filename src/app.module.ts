812613e2673baec0255c1d300f4fd672eafe21d1
Denta Muhajir <denta.muhajir@binus.ac.id>
2026-01-10 14:42:42 +0700

    Configure database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [
      ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () : TypeOrmModuleOptions  => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        schema: process.env.DB_SCHEMA,
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
