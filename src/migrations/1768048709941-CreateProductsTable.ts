16898c9d99f2182196bdf9856a93f4de087fe99c
Denta Muhajir <denta.muhajir@binus.ac.id>
2026-01-10 19:39:57 +0700

    Setup migration & execute

---
+++ src/migrations/1768048709941-CreateProductsTable.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateProductsTable1768048709941 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
