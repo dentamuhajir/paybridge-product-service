68684b42f4180222ea98566de7371fb593072eff
Denta Muhajir <denta.muhajir@binus.ac.id>
2026-01-31 17:07:20 +0700

    add logger loan tenor controller

--- src/modules/loan-product/controllers/loan-tenor.internal.controller.ts
+++ src/modules/loan-product/controllers/loan-tenor.internal.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseUUIDPipe, UseGuards } from "@nestjs/common";
+import { Controller, Get, Logger, Param, ParseUUIDPipe, UseGuards } from "@nestjs/common";
 import { LoanTenorService } from "../services/loan-tenor.service";
 import { InternalTokenGuard } from "../../../infrastructure/security/internal-token.guard";
 import { ApiResponse } from "src/shared/response/api-response";
@@ -6,13 +6,18 @@ import { ApiResponse } from "src/shared/response/api-response";
 @Controller("internal/loan-tenors")
 @UseGuards(InternalTokenGuard)
 export class LoanTenorInternalController {
+  private readonly logger = new Logger(LoanTenorInternalController.name);
   constructor(private readonly service: LoanTenorService) {}
 
   @Get(":id")
   async getById(
     @Param("id", new ParseUUIDPipe({ version: "4" })) id: string,
   ) {
+    this.logger.log(`GET /internal/loan-tenors/${id} called`);
+    
     const data = await this.service.getTenorDetail(id);
+
+    this.logger.log(`Loan tenor ${id} found`);
     return ApiResponse.success("Loan tenor found", data);
   }
 }
